4474cfb9cbd752638da0cac9afd014785efcc021
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-08-03 18:16:35 -0500

    Minor cleanup in the entrypoint script
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,12 @@
 # Common helpers
 ########################################################################################################################
 # Err on anything
+# Note: bluetoothd might fail
 set -e
 
-helpers::avahi(){
+helpers::dbus(){
   # On container restart, cleanup the crap
   rm -f /var/run/dbus/pid
-  rm -f /run/avahi-daemon/pid
-
   # Not really useful, but then
   dbus-uuidgen --ensure
 
@@ -19,6 +18,11 @@ helpers::avahi(){
   until [ -e /var/run/dbus/system_bus_socket ]; do
     sleep 1s
   done
+}
+
+helpers::avahi(){
+  # On container restart, cleanup the crap
+  rm -f /run/avahi-daemon/pid
 
   # Set the hostname, if we have it
   sed -i'' -e "s,%AVAHI_NAME%,$AVAHI_NAME,g" /etc/avahi/avahi-daemon.conf
@@ -49,13 +53,13 @@ helpers::createUser(){
 
 # On first run
 if [ ! -e ".first-run" ]; then
-
-  echo "First run configuration"
+  printf "First run configuration\n"
 
   # Create a group
-  echo "Group and permissions setup"
+  printf "Group and permissions setup\n"
   groupadd afp-share
 
+  # Create directories if not there
   [ -d "$HOME_ROOT" ]        || mkdir "$HOME_ROOT"
   [ -d "$SHARE_ROOT" ]       || mkdir "$SHARE_ROOT"
   [ -d "$TIMEMACHINE_ROOT" ] || mkdir "$TIMEMACHINE_ROOT"
@@ -63,29 +67,31 @@ if [ ! -e ".first-run" ]; then
   USERS=($USERS)
   PASSWORDS=($PASSWORDS)
 
-  echo "Creating users"
+  printf "Creating users\n"
   for ((index=0; index<${#USERS[@]}; index++)); do
     helpers::createUser "${USERS[$index]}" "${PASSWORDS[$index]}"
   done
 
+  # Set directories ownership + sticky
   chown "${USERS[0]}:afp-share" "$SHARE_ROOT"
   chmod g+swrx "$SHARE_ROOT"
   chown "${USERS[0]}:afp-share" "$TIMEMACHINE_ROOT"
   chmod g+swrx "$TIMEMACHINE_ROOT"
 
+  # Set config
   sed -i'' -e "s,%NAME%,$NAME,g" /etc/afp.conf
-
   sed -i'' -e "s,%HOME_ROOT%,$HOME_ROOT,g" /etc/afp.conf
   sed -i'' -e "s,%SHARE_ROOT%,$SHARE_ROOT,g" /etc/afp.conf
   sed -i'' -e "s,%TIMEMACHINE_ROOT%,$TIMEMACHINE_ROOT,g" /etc/afp.conf
 
   touch .first-run
-  echo "Done with first run, all set"
+  printf "Done with first run, all set\n"
 fi
 
-# If there is no AVAHI_NAME, use the AFP service name?
+# If there is no AVAHI_NAME, use the AFP service name (XXX not too sure about that)
 AVAHI_NAME=${AVAHI_NAME:-$NAME}
 
 # Run with it
+helpers::dbus
 helpers::avahi
 exec netatalk -d -F /etc/afp.conf "$@"
